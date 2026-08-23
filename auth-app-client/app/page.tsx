ee4c62d371a8d81cf6b47545ce17180b78af32b6
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-08-24 01:05:55 +0300

    add login page ui design

--- auth-app-client/app/page.tsx
+++ auth-app-client/app/page.tsx
@@ -1,69 +1,83 @@
+import LoginFormComponent from "@/components/LoginFormComponent";
 import Image from "next/image";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <section className="bg-amber-50 flex items-center md:justify-center h-screen lg:p-10">
+
+      {/* lg auth login image */}
+
+      <div className="hidden lg:block">
+
         <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
+          src={'/next.svg'}
+          alt="auth app logo"
+          height={100}
           width={100}
-          height={20}
-          priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
+
+        <div className="flex justify-evenly items-end">
+
+
+          <h1 className="text-4xl font-bold w-40">
+            Explore the things <span className="text-blue-500">you love</span>.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+
+
+          <div className="lg:150 xl:w-150">
             <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
+              src={'/login-lg-screen-img.png'}
+              alt="auth app login image"
+              width={1000}
+              height={1000}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
         </div>
-      </main>
-    </div>
+      </div>
+
+      {/* lg border */}
+      <div className="hidden lg:block lg:border border-gray-300 h-full" />
+
+
+      {/* login form */}
+      <div className="mx-auto lg:mx-8 w-[80%] md:w-[60%] lg:w-full">
+
+        <div className="block mb-10 lg:hidden">
+          <Image
+            src={'/next.svg'}
+            alt="auth app logo"
+            height={100}
+            width={100}
+            className="mx-auto"
+          />
+        </div>
+
+        <h3 className="text-lg text-center lg:text-start">Log in to Auth App</h3>
+
+        <LoginFormComponent />
+
+        {/* <Link
+          href={'/forget-password'}
+          className="inline-block my-5 text-center  w-full py-3 rounded-full cursor-pointer hover:bg-amber-100"
+        >Forgotten password?</Link> */}
+        <Link
+          href={'/register'}
+          className="inline-block my-5 text-center border border-blue-500 text-blue-500 w-full py-3 rounded-full cursor-pointer hover:bg-blue-100"
+        >Create new account</Link>
+
+        <div className="flex justify-center">
+          <Image
+            src="/next.svg"
+            alt="auth app logo"
+            width={70}
+            height={70}
+          />
+        </div>
+
+      </div>
+
+    </section>
   );
 }
