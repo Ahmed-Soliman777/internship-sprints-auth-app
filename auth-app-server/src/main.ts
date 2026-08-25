1b8111bdcce1c93ab211af4b3c591abc9fe00662
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-08-25 15:41:47 +0300

    add cors and make client connects to server

--- auth-app-server/src/main.ts
+++ auth-app-server/src/main.ts
@@ -2,9 +2,15 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,6 +18,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
